055dcc96178beb5027ded274a64ec259624e59d6
Mou <m.mounir.f@gmail.com>
2024-01-20 00:09:26 +0100

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,4 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["embla-carousel-react"],
-    },
-  },
 });
